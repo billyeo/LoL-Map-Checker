fbf147c97b3e760012fcfb2cfc001139e7164a46
DGamesta <sonofagun72@gmail.com>
2017-10-13 21:31:07 -0400

    Made the actual website work with the user lookup, and cleaned up a lot of code

--- sample.js
+++ sample.js
@@ -1,5 +1,5 @@
-console.log('Version -0.0002');
-function summonerLookUp() {
+console.log('Version -0.0003');
+function testingLookUp() {
     console.log('this works');
     var SUMMONER_NAME = "";
     SUMMONER_NAME = $("#userName").val();
@@ -19,31 +19,6 @@ function summonerLookUp() {
 
             },
             success: function (json) {
-                //console.log(json);
-                //var SUMMONER_NAME_NOSPACES = SUMMONER_NAME.replace(" ", "");
-
-                //SUMMONER_NAME_NOSPACES = SUMMONER_NAME_NOSPACES.toLowerCase().trim();
-
-
-                //summonerLevel = json[SUMMONER_NAME_NOSPACES].summonerLevel;
-                // summonerID = json[SUMMONER_NAME_NOSPACES].id;
-                // accountID = json[SUMMONER_NAME_NOSPACES].accountId;
-                // profileICON = json[SUMMONER_NAME_NOSPACES].profileIconId;
-              
-
-                //document.getElementById("sLevel").innerHTML = summonerLevel;
-                // document.getElementById("sID").innerHTML = summonerID;
-                // document.getElementById("sAccID").innerHTML = accountID;
-                // document.getElementById("proIcon").innerHTML = profileICON;
-
-
-
-                //var temp = json;
-                //var temp2= JSON.stringify(json); //worked 
-                //var pleaseWork = JSON.parse(json);
-                //console.log(pleaseWork);
-                
-                
                 //Loop to search for a Champion kill, then print its json
                 for (i=0; i<json.frames.length; i++)
                 {
@@ -56,10 +31,6 @@ function summonerLookUp() {
                 }
             }
 
-
-
-
-
             },
             error: function (XMLHttpRequest, textStatus, errorThrown) {
                 alert("error getting Summoner data!");
@@ -69,3 +40,49 @@ function summonerLookUp() {
 
     } else {}
 }
+
+
+
+function summonerLookUp(){
+	var SUMMONER_NAME = "";
+    SUMMONER_NAME = $("#userName").val();
+
+    var API_KEY = "";
+    API_KEY = $("#API-Key").val();
+
+    if (SUMMONER_NAME !== "") {
+
+        $.ajax({
+            url: 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + SUMMONER_NAME + '?api_key=' + API_KEY,
+            type: 'GET',
+            dataType: 'json',
+            data: {
+
+            },
+            success: function (json) {
+                //getting data from json into local variables
+                summonerLevel = json.summonerLevel;
+                summonerID = json.id;
+                accountID = json.accountId;
+                profileICON = json.profileIconId;
+              
+                //writing to website this shit
+                document.getElementById("sLevel").innerHTML = summonerLevel;
+                document.getElementById("sID").innerHTML = summonerID;
+                document.getElementById("sAccID").innerHTML = accountID;
+                document.getElementById("proIcon").innerHTML = profileICON;
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("error getting Summoner data!");
+            }
+        });
+
+
+    } else {}
+
+
+}
+
+
+
+
