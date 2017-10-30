bd022f8f1ba52c2acd1009f0869fc678a697fb4c
doubleyip <syip428@gmail.com>
2017-10-30 11:48:32 -0400

    php script for database connection

--- dbtestcon.php
+++ dbtestcon.php
@@ -7,12 +7,12 @@ $username="b2478bf1068eea";
 $password="ed2bdf4f";
 $db="heroku_826d2573636328d";
 $tablename="lolusers";
-$column="username";
+$column="matchid";
 
 mysql_connect($hostname,$username,$password);
 mysql_select_db($dbname);
 
-//test to access tables to check for data username column
+//test to access tables to check for data matchid column and print out data
 $query="SELECT * FROM $tablename";
 $data=mysql_query($query);
 
